3c35c73dcec2be19b5ca1bb784cd3ac402df7545
Gyenh <blazeog@hotmail.com>
2018-09-08 10:18:37 +0200

    Admin resgistration, redirect to new mob, creating event redirect to root

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -49,7 +49,7 @@ class Admins::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -62,9 +62,9 @@ class Admins::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    "/mobs/new"
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :user_events
-  root 'home_pages#index'
 
-  resources :events
+  root 'home_pages#index'
 
   devise_for :admins, path: 'admins', controllers: { sessions: 'admins/sessions', registrations: 'admins/registrations' }
   # path example : http://localhost:3000/admins/sign_in
 
   devise_for :users, path: 'users', controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   # path example : http://localhost:3000/users/sign_in
+
+  resources :user_events
+
+  resources :events
 
   resources :mobs
 
