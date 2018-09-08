45976eec051cde1b91011d3d3b302b0c55c8729e
Cathleen HENNINOT <cathleenhenninot@MacBook-Pro-de-Cathleen.local>
2018-09-08 11:34:31 +0200

    pour pull gino

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,7 @@ Rails.application.routes.draw do
   devise_for :users, path: 'users', controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   # path example : http://localhost:3000/users/sign_in
 
-
-
-
   resources :mobs
+
 
 end
