e0026e8a894fa29a58bfd61f876fe2a76784f5ed
Gyenh <blazeog@hotmail.com>
2018-09-09 20:41:21 +0200

    Adding firstname and lastname forms in the signup

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Admins::RegistrationsController < Devise::RegistrationsController
   include Accessible
   skip_before_action :check_user, except: %i[new create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -76,6 +77,9 @@ class Admins::RegistrationsController < Devise::RegistrationsController
     "/mobs/new"
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
