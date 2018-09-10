68b34547e9a95640762d30e4dcd5d016441e8dbf
Cathleen HENNINOT <cathleenhenninot@MacBook-Pro-de-Cathleen.local>
2018-09-10 12:38:39 +0200

    changement seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,12 +18,12 @@ User.destroy_all
 Mob.destroy_all
 
 mob_name = ['Les mégots', 'Mister Clean', 'Green broom']
-event_name = ['Les mégots, louvre', 'Mister Clean, C.I.D.J', 'Green broom']
+event_name = ['Les mégots', 'Mister Clean', 'Green broom']
 
 phone = %w[0646789465 0764859856 0715326548]
 description = ['Nous déployons d’importants moyens humains et matériels pour assurer la propreté de l’espace public.', 'pour rendre nos villes plus propres et plus respirable. Vous êtes toujours les bienvenus dans notre communauté de cleaners  dans le monde entier !', 'NOus sommes une cinquantaine d\'ambassadeurs de la propreté au service de notre ville.']
 description_dechet = ['mégots', 'sac plastique', 'déchets']
-adress = ['91 Rue de Rivoli, 75001 Paris', '101 Quai Branly, 75015 Paris', 'cacabouya']
+adress = ['91 Rue de Rivoli, 75001 Paris', '101 Quai Branly, 75015 Paris', '96 rue de Bercy, 75012 Paris']
 # userEvent = Test.create( event: '22', user: '65') #testasup
 
 # user2 = User.create! :last_name 'hehe', :first_name => 'John Doe', :email => 'john2@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
@@ -36,7 +36,7 @@ adress = ['91 Rue de Rivoli, 75001 Paris', '101 Quai Branly, 75015 Paris', 'caca
 
   mob = Mob.create(name: mob_name[i], phone: phone[i], web_url_1: Faker::Internet.url, web_url_2: Faker::Internet.url, description: description[i])
 
-  event = Event.create(name: event_name[i], place: "#{adress[i]}", description: "Nous allons nous concentrer sur le rammasage de #{description_dechet[i]}", date: "2018-09-24", hour: "18h00", mob_id: mob.id)
+  event = Event.create(name: event_name[i], place: "#{adress[i]}", description: "Nous allons nous concentrer sur le ramassage de #{description_dechet[i]}, in hendrerit in vulputate velit eseros et accumsan et iusto odio dignissim qui blandit praesent...", date: "2018-09-24", hour: "18h00", mob_id: mob.id)
 
 #cette ligne inscrit tous les user a un evenement
   # userEvent = UserEvent.create(user_id: user.id, event_id: event.id)
