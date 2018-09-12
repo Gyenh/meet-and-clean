9b081e8db7e2a55bb9c893eec74378ea81874f22
Gyenh <blazeog@hotmail.com>
2018-09-12 16:18:24 +0200

    Ajout de validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :mob, optional: true
   has_many :userevents
+
+  # Validation des formulaires
+  validates :name, length: {minimum: 2, maximum: 150}
+  validates :place, :length => { :maximum => 200 }
   validates :description, :length => { :maximum => 450 }
 
 end
--- app/models/mob.rb
+++ app/models/mob.rb
@@ -1,4 +1,12 @@
 class Mob < ApplicationRecord
   has_many :events
   has_many :admins
+
+  # Validation des formulaires
+  validates :description, :length => { :maximum => 450 }
+  validates :name, uniqueness: true, length: {minimum: 2, maximum: 150}
+  validates :phone, :length => { :maximum => 15 }, numericality: { only_integer: true }
+  validates :web_url_1, :length => { :maximum => 80 }
+  validates :web_url_2, :length => { :maximum => 80 }
+
 end
