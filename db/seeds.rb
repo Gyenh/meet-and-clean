b63a32c0c06b12257e7bc91f3ef903bdecc5a323
kwevan gouacide <kyg972@msn.com>
2018-09-12 14:16:37 +0200

    modif seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,9 +15,9 @@ Admin.destroy_all
   pwd = 123456
 
   user = User.create(last_name: Faker::Name.last_name, first_name: name, email: Faker::Internet.free_email(name), password: pwd, password_confirmation: pwd)
-  mob = Mob.create(name: SeedObject.get_mob_name[i], phone: SeedObject.get_phone[i], web_url_1: Faker::Internet.url, web_url_2: Faker::Internet.url, description: SeedObject.get_description_mob[i])
-  event = Event.create(name: SeedObject.get_event_name[i], place: SeedObject.get_adress[i], description: SeedObject.get_description_event[i], date: SeedObject.get_event_date[i], hour: SeedObject.get_event_hour[i], mob_id: mob.id)
-  admin = Admin.create(email: Faker::Internet.free_email , first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, admin: nil, mob_id: mob.id, password: pwd, password_confirmation: pwd)
+  mob = Mob.create(name: SeedObject.get_mob_name[i], phone: SeedObject.get_phone[i], web_url_1: Faker::Internet.url, web_url_2: Faker::Internet.url, description: SeedObject.get_description_mob[i], profile_url: "https://img3.closermag.fr/var/closermag/storage/images/1/2/5/9/1/12591000/hailie-fille-eminem-toujours-sexy-sur-instagram_exact1024x768_l.jpg")
+  event = Event.create(name: SeedObject.get_event_name[i], place: SeedObject.get_adress[i], description: SeedObject.get_description_event[i], date: SeedObject.get_event_date[i], hour: SeedObject.get_event_hour[i], mob_id: mob.id, banner_url: "https://img3.closermag.fr/var/closermag/storage/images/1/2/5/9/1/12591000/hailie-fille-eminem-toujours-sexy-sur-instagram_exact1024x768_l.jpg")
+  admin = Admin.create(email: Faker::Internet.free_email , first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, mob_id: mob.id, password: pwd, password_confirmation: pwd)
 
   #cette ligne inscrit tous les user a un evenement
   # userEvent = UserEvent.create(user_id: user.id, event_id: event.id)
