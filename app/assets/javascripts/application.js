d039b27f4a550ad039b97da018cd76e50b971624
Gyenh <blazeog@hotmail.com>
2018-09-12 14:48:29 +0200

    Fix a merge issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,31 +17,19 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
-
-
  //début du code de la map
-
-
-var ny = gon.mapLatLong;
-var name = gon.mapName;
+ var ny = gon.mapLatLong;
+  var name = gon.mapName
 // création de la map
-
 var map = L.map('map', {zoomControl:false}).setView(ny, 16);
-	map.dragging.disable();
+map.dragging.disable();
 // if(map==null)then map=new L.Map('idopenstreet').setView();
 // création du calque images
-L.tileLayer(
-	'http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}',
-	{
-	maxZoom: 20
-	}
-).addTo(map);
-
+L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}', {
+  maxZoom: 20
+}).addTo(map);
 // ajout d'un markeur
 var marker = L.marker(ny).addTo(map);
-
 // ajout d'un popup
 marker.bindPopup(name);
-
 //fin du code de la map
