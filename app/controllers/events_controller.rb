9b792c5f2d3f4ded1a09e77da8adfb953087a031
Eang Ly <eangly.eng@gmail.com>
2018-09-13 12:17:29 +0200

    en local : add img on event banner by Admin w/AS

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -135,6 +135,6 @@ class EventsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def event_params
-    params.require(:event).permit(:name, :place, :description, :date, :hour)
+    params.require(:event).permit(:name, :place, :description, :date, :hour, :banner)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :mob, optional: true
   has_many :userevents
+  has_one_attached :banner
+
 
 
   validates :name, presence: true, length: { maximum: 15 }
