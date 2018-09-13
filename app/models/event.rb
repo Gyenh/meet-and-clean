c598eda513854896c1dbaaa7803a9dc598a5046c
Gyenh <blazeog@hotmail.com>
2018-09-13 15:10:36 +0200

    Cleaning, trying to have a proper code v.0.3.1

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,14 +6,17 @@ class Event < ApplicationRecord
   validates :description, length: { maximum: 450 }
 
   validate :adress_validation
+  # Cette validation appelle notre propre validation maison
 
   def adress_validation
     if adress_verif(place)
+    # Si l'adresse est reconnue par GeoCoder, alors il y a creation de l'event
     else
       errors.add('Erreur, ', 'adresse non reconnue')
     end
   end
 
+  # Methode qui verifie avec l'API GeoCoder
   def adress_verif(adress)
     results = Geocoder.search(adress)
     true
