5106684a3112cbb2fe112444adae9115175a1b4a
Gyenh <blazeog@hotmail.com>
2018-09-13 16:58:05 +0200

    fix an issue on social email button

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook email linkedin)
+  config.allow_sites = %w[twitter facebook email]
 end
