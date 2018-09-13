b4148784f4de2fd2afcaf03af3bbb1fa033d79d9
Gyenh <blazeog@hotmail.com>
2018-09-13 15:05:14 +0200

    Cleaning, trying to have a proper code v.0.3

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,26 +1,23 @@
+# frozen_string_literal: true
+
 class Event < ApplicationRecord
   belongs_to :mob, optional: true
   has_many :userevents
-  validates :description, :length => { :maximum => 450 }
+  validates :description, length: { maximum: 450 }
 
   validate :adress_validation
 
   def adress_validation
-    if  adress_verif(place)
+    if adress_verif(place)
     else
-      errors.add("Erreur, ", "adresse non reconnue")
+      errors.add('Erreur, ', 'adresse non reconnue')
     end
   end
 
   def adress_verif(adress)
-    begin
-      results = Geocoder.search(adress)
-      return true
-    rescue Exception
-      return false
-    end
+    results = Geocoder.search(adress)
+    true
+  rescue Exception
+    false
   end
-
-
-
 end
